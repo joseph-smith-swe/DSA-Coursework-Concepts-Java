e1bba19387d718f343b3afd0d30153776fdf783e
joseph-smith-swe <joseph.smith.umd@gmail.com>
2024-06-05 18:05:00 -0400

    first commit

---
+++ src/DSA_With_Java/Main.java
@@ -0,0 +1,37 @@
+package DSA_With_Java;
+
+import DSA_With_Java.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+        System.out.println("////////////////Linked List///////////");
+        LinkedList myLL = new LinkedList(2);
+        System.out.println(myLL);//Here we get only a memory address
+
+//        myLL.getHead();
+//        myLL.getTail();
+//        myLL.getLength();
+//        myLL.printList();
+//
+//        myLL.append(3);
+        myLL.append(1);
+//        myLL.prepend(1);
+//        myLL.printList();
+//        System.out.println("/////////Returns from removeLast() method invocations///////////");
+//        //(2) Items -Returns 2 Nodes
+//        System.out.println(myLL.removeLast().value);
+//        // (1) Item -Returns 1 Node
+//        System.out.println(myLL.removeLast().value);
+//        // (0) Items - Returns null
+//        System.out.println(myLL.removeLast());
+
+        System.out.println("////Returns from removeFirst()////");
+        //(2) Items - Return 2 Node
+        System.out.println(myLL.removeFirst().value);
+        //(1) Item - Returns 1 Node
+        System.out.println(myLL.removeFirst().value);
+        //(0) Items - Returns null
+        System.out.println(myLL.removeFirst());
+    }
+}
